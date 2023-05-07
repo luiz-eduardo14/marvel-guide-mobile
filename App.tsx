0a8d455027475f61b5fe42845ea3517e2b2caec9
luiz-eduardo14 <masterdudu123@hotmail.com>
2023-05-07 14:18:21 -0300

    feat: implement queryClientProvider of react query in project

--- App.tsx
+++ App.tsx
@@ -6,6 +6,7 @@ import {
   HStack,
   Center,
 } from 'native-base';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
 import { NavigationContainer } from '@react-navigation/native';
 import Home from './src/features/home';
@@ -24,28 +25,32 @@ const theme = extendTheme({
   },
 });
 
+const queryClient = new QueryClient();
+
 export default function App() {
   return (
-    <NavigationContainer>
-      <NativeBaseProvider theme={theme}>
-        <Box flex={1} flexDirection="column">
-          <HStack safeArea bg="red.600" flex={0.2}>
-            <Box width="100%">
-              <Center
-                justifyContent="center"
-                alignItems="center"
-                marginLeft="auto"
-                flexDirection="row"
-                width="100%"
-                marginBottom={0}
-              >
-                <MarvelIcon size={180} />
-              </Center>
-            </Box>
-          </HStack>
-          <Home />
-        </Box>
-      </NativeBaseProvider>
-    </NavigationContainer>
+    <QueryClientProvider client={queryClient}>
+      <NavigationContainer>
+        <NativeBaseProvider theme={theme}>
+          <Box flex={1} flexDirection="column">
+            <HStack safeArea bg="red.600" flex={0.2}>
+              <Box width="100%">
+                <Center
+                  justifyContent="center"
+                  alignItems="center"
+                  marginLeft="auto"
+                  flexDirection="row"
+                  width="100%"
+                  marginBottom={0}
+                >
+                  <MarvelIcon size={180} />
+                </Center>
+              </Box>
+            </HStack>
+            <Home />
+          </Box>
+        </NativeBaseProvider>
+      </NavigationContainer>
+    </QueryClientProvider>
   );
 }
