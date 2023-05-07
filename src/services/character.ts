d7d7f46b60a69867721e1cef6d60d15513b48a3a
luiz-eduardo14 <masterdudu123@hotmail.com>
2023-05-07 14:18:40 -0300

    feat: add request for get all characters

--- src/services/character.ts
+++ src/services/character.ts
@@ -3,9 +3,14 @@ import { PaginateResponse } from '../types/paginate';
 import { CharacterEntity } from './types/character';
 import api from '../utils/api.config';
 
-export const getCharactersPaginate = async (
+const getCharactersPaginate = async (
   page: number,
 ): Promise<AxiosResponse<PaginateResponse<CharacterEntity>>> =>
   await api.get<PaginateResponse<CharacterEntity>>(
     `/character/paginate?page=${page}`,
   );
+
+const getAllCharacters = async (): Promise<AxiosResponse<CharacterEntity[]>> =>
+  await api.get('/character');
+
+export { getCharactersPaginate, getAllCharacters };
