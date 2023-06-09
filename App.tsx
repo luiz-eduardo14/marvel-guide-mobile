f9437cf50c9e01b4e756c5c0e9a16e964af5ff3c
luiz-eduardo14 <masterdudu123@hotmail.com>
2023-06-09 01:22:51 -0300

    feat: apply component Routes for unify routes of application

--- App.tsx
+++ App.tsx
@@ -1,11 +1,7 @@
-import { NativeBaseProvider, extendTheme } from 'native-base';
+import { extendTheme } from 'native-base';
 import { QueryClient, QueryClientProvider } from 'react-query';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
-import { NavigationContainer } from '@react-navigation/native';
-import Home from './src/features/home';
 import { LogBox } from 'react-native';
-import { Screen } from './src/interfaces/screen.enum';
+import { Routes } from './src/routes';
 LogBox.ignoreLogs(['Setting a timer for a long period of time']);
 
 // Define the config
@@ -23,23 +19,10 @@ const theme = extendTheme({
 
 const queryClient = new QueryClient();
 
-const Stack = createNativeStackNavigator();
-
 export default function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <NavigationContainer>
-        <NativeBaseProvider theme={theme}>
-          <Stack.Navigator
-            initialRouteName={Screen.HOME}
-            screenOptions={{
-              headerShown: false,
-            }}
-          >
-            <Stack.Screen name={Screen.HOME} component={Home} />
-          </Stack.Navigator>
-        </NativeBaseProvider>
-      </NavigationContainer>
+      <Routes theme={theme} />
     </QueryClientProvider>
   );
 }
