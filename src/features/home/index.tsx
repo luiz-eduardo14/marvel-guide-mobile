3152fe628aa87eeffb40ce7c77c506c520a2e2a1
luiz-eduardo14 <masterdudu123@hotmail.com>
2023-06-13 00:13:19 -0300

    feat: removed scrool view for apply flatList for performance in render large list

--- src/features/home/index.tsx
+++ src/features/home/index.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-misused-promises */
-import { Box, Center, ScrollView } from 'native-base';
+import { Box, Center, FlatList, ScrollView } from 'native-base';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { useCallback } from 'react';
 import { getCharactersPaginate } from '../../services/character';
@@ -7,6 +7,9 @@ import { CardCharacter } from './components/CardCharacter';
 import { useInfiniteQuery } from 'react-query';
 import { MarvelHeader } from './components/MarvelHeader';
 import { MarvelFontWrapper } from '../../utils/FontLoad';
+import { NavigationProp, useNavigation } from '@react-navigation/native';
+import { Screen } from '../../interfaces/screen.enum';
+import { RootStackParamList } from '../../interfaces/PropsRootStack';
 
 export default function Home() {
   const paginateCharacters = useCallback(async (page: number) => {
@@ -25,32 +28,39 @@ export default function Home() {
     getNextPageParam: (lastPage) => (lastPage?.meta?.currentPage ?? 0) + 1,
   });
 
+  const { navigate } = useNavigation<NavigationProp<RootStackParamList>>();
+
   return (
     <MarvelFontWrapper>
       <Center bg={'red.600'} px={4} flex={1} safeArea>
         <MarvelHeader />
         <Box flex={0.75}>
           <SafeAreaView>
-            <ScrollView
-              onMomentumScrollEnd={() => {
+            <FlatList
+              horizontal={false}
+              onEndReached={() => {
                 if (hasNextPage && !isFetchingNextPage) {
                   fetchNextPage();
                 }
               }}
+              maxToRenderPerBatch={4}
               showsVerticalScrollIndicator={false}
               showsHorizontalScrollIndicator={false}
-            >
-              {dataCharacters?.pages
-                ?.flatMap((value) => value.items)
-                .map((character) => (
-                  <CardCharacter
-                    description={character.description}
-                    imageURL={character.imageURL}
-                    title={character.name}
-                    key={character.id_origin}
-                  />
-                ))}
-            </ScrollView>
+              data={dataCharacters?.pages?.flatMap((value) => value.items)}
+              keyExtractor={(item) => item.id_origin.toString()}
+              removeClippedSubviews
+              renderItem={({ item: character }) => (
+                <CardCharacter
+                  description={character.description}
+                  imageURL={character.imageURL}
+                  title={character.name}
+                  key={character.id_origin}
+                  onPress={() => {
+                    navigate(Screen.CHARACTER, character);
+                  }}
+                />
+              )}
+            />
           </SafeAreaView>
         </Box>
       </Center>
