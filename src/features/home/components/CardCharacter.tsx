c2542da70b6fa0b196024af48cd00d62ef5d8e47
luiz-eduardo14 <masterdudu123@hotmail.com>
2023-06-13 00:11:33 -0300

    feat: added pressable in component for apply correct shape for event onPress

--- src/features/home/components/CardCharacter.tsx
+++ src/features/home/components/CardCharacter.tsx
@@ -1,38 +1,48 @@
-import { Center, Heading, Image, Text, VStack, View } from 'native-base';
+import {
+  Center,
+  Heading,
+  Image,
+  Pressable,
+  Text,
+  VStack,
+  View,
+} from 'native-base';
 import { CardCharacterProps } from './types/CardCharacter';
 
 export function CardCharacter({
   title,
-  description,
   imageURL,
+  onPress,
 }: CardCharacterProps) {
   return (
-    <VStack paddingBottom={4}>
-      <Heading textAlign="center" paddingBottom={2} justifyContent={'center'}>
-        <View>
-          <Text
-            fontSize="3xl"
-            fontWeight={'black'}
-            fontFamily={'Bebas-Regular'}
-            color={'white'}
-          >
-            {title}
-          </Text>
-        </View>
-      </Heading>
-      <Center>
-        <Image
-          resizeMethod="resize"
-          source={{
-            uri: imageURL,
-            method: 'GET',
-          }}
-          alt={'image'}
-          width={64}
-          height={48}
-          borderRadius="lg"
-        />
-      </Center>
-    </VStack>
+    <Pressable onPress={onPress}>
+      <VStack paddingBottom={4}>
+        <Heading textAlign="center" paddingBottom={2} justifyContent={'center'}>
+          <View>
+            <Text
+              fontSize="3xl"
+              fontWeight={'black'}
+              fontFamily={'Bebas-Regular'}
+              color={'white'}
+            >
+              {title}
+            </Text>
+          </View>
+        </Heading>
+        <Center>
+          <Image
+            resizeMethod="resize"
+            source={{
+              uri: imageURL,
+              method: 'GET',
+            }}
+            alt={'image'}
+            width={64}
+            height={48}
+            borderRadius="lg"
+          />
+        </Center>
+      </VStack>
+    </Pressable>
   );
 }
--- src/features/home/components/types/CardCharacter.ts
+++ src/features/home/components/types/CardCharacter.ts
@@ -2,4 +2,5 @@ export interface CardCharacterProps {
   title: string;
   imageURL: string;
   description: string | null;
+  onPress: () => void;
 }
